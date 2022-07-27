bce5b53a325e8db48e83ab266c06c37eb892a771
Yochran <71285258+Yochran@users.noreply.github.com>
2022-07-27 17:45:09 -0400

    Add files via upload

---
+++ commands/ModstatsCommand.js
@@ -0,0 +1,59 @@
+const { Permissions } = require("discord.js");
+
+const CustomEmbed = require("../utils/CustomEmbed.js");
+const Logs = require("../punishments/Logs.js");
+
+module.exports = {
+    name: "modstats",
+    alias: [],
+    execute(message, args) {
+        // check for permission
+        if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) return;
+
+        // author variable
+        const author = message.author;
+
+        // create embed
+        const embed = new CustomEmbed(CustomEmbed.getDefaults(author));
+
+        // check argument length
+        if (args.length != 1) {
+            embed.field["description"] = "Incorrect Usage!\n(`>modlogs <@user>`)"
+            message.channel.send({ embeds: [embed.create()] });
+            return;
+        }
+
+        // check if argument 1 is a valid user
+        const target = message.mentions.members.first();
+
+        if (!target) {
+            embed.field["description"] = "Invalid Member!\n(`Must be a ping`)"
+            message.channel.send({ embeds: [embed.create()] });
+            return;
+        }
+
+        // get stats
+        const stats = Logs.getStats(message.guild.id, target.user.id);
+
+        if (stats) {
+            // get total stats
+            const total = stats.WARN + stats.MUTE + stats.KICK + stats.JAIL + stats.BAN;
+
+            // create fields
+            const fields = [
+                {name: "Warns", value: stats.WARN.toString(), inline: true},
+                {name: "Mutes", value: stats.MUTE.toString(), inline: true},
+                {name: "Kicks", value: stats.KICK.toString(), inline: true},
+                {name: "Jails", value: stats.JAIL.toString(), inline: true},
+                {name: "Bans", value: stats.BAN.toString(), inline: true},
+            ]
+
+            // update embed
+            embed.field["description"] = "<@" + target.user.id + "> has taken `" + total + "` actions."; 
+            embed.field["fields"] = fields;
+
+            // send message
+            message.channel.send({ embeds: [embed.create()] });
+        }
+    }
+}
